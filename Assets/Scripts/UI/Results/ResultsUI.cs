4c8e90416927ca5aaff5b907e44e11409b9a700c
Cristian Rios <40129020+sanxxtiago@users.noreply.github.com>
2026-06-22 18:03:33 -0500

    Revert "Add emoji spritesheet; update scenes & scripts"
    This reverts commit 9afabd27f057270a4e468e6776682907aae49bd0.

--- Assets/Scripts/UI/Results/ResultsUI.cs
+++ Assets/Scripts/UI/Results/ResultsUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ResultsUI : MonoBehaviour
