7768ed8dd52a661fe7e151bbc6c32f8dd9ee8ee0
Cristian Rios <40129020+sanxxtiago@users.noreply.github.com>
2026-06-29 22:40:00 -0500

    Unity Update
    Unity project migration

---
+++ Assets/Scripts/Gameplay/Hunter/HunterExercise.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HunterExercise : ExerciseController
+{
+    protected override void OnExerciseStart()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    
+}
